750ef926dbcd2ab7876074fdb1fbc709d2931c06
Elizabeth Sanger <libsang@gmail.com>
2017-01-25 10:17:32 -0600

    first commit

---
+++ WebApplication1/Scripts/app.js
@@ -0,0 +1,46 @@
+﻿"use strict";
+var app = angular.module("BangazonToDo", ["ngRoute"])
+.constant('API_URL', 'localhost://5000/UserTask');
+
+
+app.config(function ($routeProvider) {
+    $routeProvider.
+        when("/", {
+            templateUrl: "partials/UserTasks.html",
+            controller: "UserTaskCtrl"
+        }).
+
+        when("/Tasks/ByStatus", {
+            templateUrl: 'partials/ByStatus.html',
+            controller: "UserTaskCtrl",
+
+        }).
+        when('/Tasks/:taskId', {
+            templateUrl: "partials/TaskDetail.html",
+            controller: "UserTaskCtrl",
+        }).
+
+        when("/Tasks/:taskId/edit", {
+            templateUrl: "partials/TaskForm.html",
+            controller: "UserTaskCtrl",
+        }).
+
+        otherwise("/");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
