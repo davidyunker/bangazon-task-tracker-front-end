1dff7f1215e8448601b22a29bfecf7a02f715513
Elizabeth Sanger <libsang@gmail.com>
2017-01-28 16:07:55 -0600

    improvements to filter status

--- WebApplication1/Scripts/Controllers/TaskController.js
+++ WebApplication1/Scripts/Controllers/TaskController.js
@@ -1,24 +1,42 @@
 ﻿app.controller("UserTaskCtrl", function ($scope, $location, TaskFactory, $routeParams) {
+    let filterTasks = function (task) {
+        if (task.status === 0) {
+            task.status = "To Do";
+        }
+
+        else if (status === 1) {
+            task.status = "In Progress";
+        }
+        else {
+            task.status = "Completed";
+        }
+    }
 
 
     TaskFactory.getTasks()
-          .then((tasks) => {
-              console.log("tasks", tasks);
-              $scope.userTasks = tasks;
-          });
+               .then((tasks) => {
+                   console.log("unfiltered tasks inside GET", tasks)
+                   tasks.forEach(function (task) {
+                       filterTasks(task)
+                       console.log('task in loop', task);
+                   })
+                   $scope.userTasks = tasks;
+               });
 
     $scope.taskDelete = (taskId) => {
         TaskFactory.deleteTask(taskId)
-        .then((response) => {
+        then((tasks) => {
             TaskFactory.getTasks()
-            .then((tasks) => {
-                $scope.userTasks = tasks;
-            });
+               .then((tasks) => {
+                   console.log("unfiltered tasks inside GET", tasks)
+                   tasks.forEach(function (task) {
+                       filterTasks(task)
+                       console.log('task in loop delete', task);
+                   })
+                   $scope.userTasks = tasks;
+               });
         });
     };
-
- 
- 
 });
 
         
